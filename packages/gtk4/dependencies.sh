3221f196cd09bba414359ead55194b6d0ee41037
Navid.Means.Promise <navid@homa-foundation.org>
2022-05-04 02:58:25 +0430

    building gtk4 package

---
+++ packages/gtk4/dependencies.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# ██╗  ██╗ ██████╗ ███╗   ███╗ █████╗
+# ██║  ██║██╔═══██╗████╗ ████║██╔══██╗
+# ███████║██║   ██║██╔████╔██║███████║
+# ██╔══██║██║   ██║██║╚██╔╝██║██╔══██║
+# ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║
+# ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝
+
+# ███████╗ ██████╗ ██╗   ██╗███╗   ██╗██████╗  █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██║   ██║████╗  ██║██╔══██╗██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+# █████╗  ██║   ██║██║   ██║██╔██╗ ██║██║  ██║███████║   ██║   ██║██║   ██║██╔██╗ ██║
+# ██╔══╝  ██║   ██║██║   ██║██║╚██╗██║██║  ██║██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+# ██║     ╚██████╔╝╚██████╔╝██║ ╚████║██████╔╝██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+# ╚═╝      ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+
+#════════════════════════════════════════════════════════════════════════════════════
+# Homa-Foundation.org
+# Description:
+#   This is a shell script module system writen by NAVID for simplicity in developing
+#   complex shell script projects
+#
+# This file released under the "GNU Affero General Public License"
+# and you are accepting the license and reponsible about that by using this file
+# Read more on: https://www.gnu.org/licenses/agpl-3.0.en.html
+
+sudo apt-get -y install libncurses5
+sudo apt-get -y install libelf++0 libelf1 libelf-dev
+sudo apt-get -y install libxml2 libxml2-dev libxml2-utils 
+sudo apt-get -y install libxslt1-dev libxslt1.1
+sudo apt-get -y install sysprof libpango1.0-dev
+sudo apt-get -y install libpango-1.0-0 libpangocairo-1.0-0
+sudo apt-get -y install libglib2.0-0 libglib2.0-dev
+sudo apt-get -y install libxkbcommon-dev libxrandr-dev libxrender-dev
+sudo apt-get -y install libxi-dev libxdamage-dev libxfixes-dev
+sudo apt-get -y install libxext-dev libxcursor-dev libfontconfig-dev
+sudo apt-get -y install libx11-dev libwayland-dev libpng-dev
+sudo apt-get -y install libc6-dev libc6 libc6-amd64 libc++-10-dev
+sudo apt-get -y install libc++-dev 
+sudo apt-get -y install libpng++-dev libpng-dev libpng16-16 
+sudo apt-get -y install libjpeg-dev libjpeg62 libjpeg8 libjpeg8-dev
+sudo apt-get -y install libspectre-dev libspectre1
+sudo apt-get -y install librsvg2-2 librsvg2-dev
+sudo apt-get -y install libpixman-1-0 libpixman-1-dev
+sudo apt-get -y install libxcb-shm0 libxcb-shm0-dev
+sudo apt-get -y install cairo-5c
+sudo apt-get -y install libicu-dev libthai-dev
+sudo apt-get -y install libvulkan-dev libvulkan1
+sudo apt-get -y install libgdk-pixbuf2.0-0 libgdk-pixbuf2.0-dev
+sudo apt-get update
+sudo apt-get -y install libegl1-mesa libegl1-mesa-dev
+sudo apt-get -y install libgles2-mesa-dev libglfw3-dev libglfw3
+sudo apt-get -y install libglew-dev libglew2.1
+sudo apt-get -y install ffmpeg libatk1.0-dev libglm-dev iso-codes
+sudo apt-get -y install libgraphene-1.0-dev libgraphene-1.0-0 
+sudo apt-get -y install sassc hicolor-icon-theme libglib2.0-0
+sudo apt-get -y install libglib2.0-dev pkg-config bison gettext
+sudo apt-get -y install xsltproc 
+sudo apt-get -y install libpython3.9 --fix-missing
+sudo apt-get -y install python3.9-full
+sudo apt-get -y install python3.9-dev
+
+
+
+
+
